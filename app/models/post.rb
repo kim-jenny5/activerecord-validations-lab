733cc7c9b653049e376f56c3460412112fb342c3
Jenny Kim <kimj1905@gmail.com>
2021-07-06 15:22:57 -0400

    commit tree 23c444a59dc680e9e17575b6307395345d0d9b59

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,16 +1,15 @@
 class Post < ActiveRecord::Base
-    # include ActiveModel::Validations
     validates :title, presence: true
     validates :content, length: { minimum: 250 }
     validates :summary, length: { maximum: 250 }
     validates :category, inclusion: { in: %w(Fiction Non-fiction) }
-    # validates_with TitleValidator
     validate :clickbait?
 
     def clickbait?
-        unless title.match?(/Won't Believe/) || title.match?(/Secret/) || title.match?(/Top \d/) || title.match?(/Guess/)
+        clickbait_arr = [/Won't Believe/,  /Secret/, /Top \d/, /Guess/]
+
+        if clickbait_arr.none? { |clickbait| clickbait.match?(title) }
             errors.add :title, "not clickbait-y enough"
         end
     end
-
 end
