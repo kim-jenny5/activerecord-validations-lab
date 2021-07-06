3cc466ecc43d98831a969d1c7ee2070a6980a12c
Jenny Kim <kimj1905@gmail.com>
2021-07-06 14:59:14 -0400

    Done.

--- app/validators/title_validator.rb
+++ app/validators/title_validator.rb
@@ -1,7 +1,8 @@
-class TitleValidator < ActiveModel::Validator
-    def validate(record)
-        unless record.title.match?(/Won't Believe/) || record.title.match?(/Secret/) || record.title.match?(/Top \d/) || record.title.match?(/Guess/)
-            record.errors.add :title, "not clickbait-y enough"
-        end
-    end
-end
+# class TitleValidator < ActiveModel::Validator
+#     def validate(record)
+#         binding.pry
+#         unless record.title.match?(/Won't Believe/) || record.title.match?(/Secret/) || record.title.match?(/Top \d/) || record.title.match?(/Guess/)
+#             record.errors.add :title, "not clickbait-y enough"
+#         end
+#     end
+# end
