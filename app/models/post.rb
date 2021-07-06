2a076e0e1f66ad3dab683784988bc6315876fe78
Jenny Kim <kimj1905@gmail.com>
2021-07-06 14:46:26 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,16 @@
 class Post < ActiveRecord::Base
+    include ActiveModel::Validations
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: %w(Fiction Non-fiction) }
+    validates_with TitleValidator
+    # validate :clickbait?
+
+    # def clickbait?
+    #     unless title.match?(/Won't Believe/) || title.match?(/Secret/) || title.match?(/Top \d/) || title.match?(/Guess/)
+    #         errors.add :title, "not clickbait-y enough"
+    #     end
+    # end
+
 end
---
+++ app/validators/title_validator.rb
@@ -0,0 +1,7 @@
+class TitleValidator < ActiveModel::Validator
+    def validate(record)
+        unless record.title.match?(/Won't Believe/) || record.title.match?(/Secret/) || record.title.match?(/Top \d/) || record.title.match?(/Guess/)
+            record.errors.add :title, "not clickbait-y enough"
+        end
+    end
+end
